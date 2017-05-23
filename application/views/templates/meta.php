d97c4a6834391d6182f0e1c0a9c65644a7ed187b
fadil_hakim <fadil.nylon@gmail.com>
2017-05-23 11:44:03 +0800

    tes commit diffrent account

--- application/views/templates/meta.php
+++ application/views/templates/meta.php
@@ -28,4 +28,5 @@
 <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Open+Sans' type='text/css'>
 <script type="text/javascript" src="<?php echo base_url('assets/js/jquery-2.1.1.min.js'); ?>"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/js/bootstrap/js/bootstrap.min.js'); ?>"></script>
+
 </head>
