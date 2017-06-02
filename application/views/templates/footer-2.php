021b2a2d7f660eea3c59dad0fb77b2c43b5e9fd9
fadil_hakim <fadil.nylon@gmail.com>
2017-06-02 11:20:32 +0800

    test new deploy

--- application/views/templates/footer-2.php
+++ application/views/templates/footer-2.php
@@ -2,7 +2,7 @@
 <hr>
       <div class="row">
           <div class="col-lg-6">
-            <h3 class="code-f" style="text-align: right">Hubungi Kami :</h3>
+            <h3 class="code-f" style="text-align: right">Hubungi Kami</h3>
           </div>
           <div class="col-lg-6">
             <div class="phone">
